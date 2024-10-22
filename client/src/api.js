2c67fb0b956049e25a9891aee47fe3017ea274ec
1Aksoni <akshat2001soni@gmail.com>
2024-10-23 01:42:01 +0530

    base url changed

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with default configurations
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // The backend URL (change if deployed)
+  baseURL: 'https://job-application-tracker-fx36.onrender.com', // The backend URL (change if deployed)
   headers: {
     'Content-Type': 'application/json',
   },
